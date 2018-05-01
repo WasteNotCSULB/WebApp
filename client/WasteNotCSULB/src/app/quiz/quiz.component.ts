6aedbe4e28b544c5acbd87d5e4763309c85bb495
patrickly <patrickkimly@gmail.com>
2018-05-01 15:49:54 -0700

    Quiz each item gets a property answered false added

--- client/WasteNotCSULB/src/app/quiz/quiz.component.ts
+++ client/WasteNotCSULB/src/app/quiz/quiz.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { RestApiService } from '../rest-api.service';
+import { DataService } from '../data.service';
+
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
@@ -7,9 +10,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizComponent implements OnInit {
 
-  constructor() { }
+  items: any = null;
+  question1: any = '';
+  objTEST: any = null;
 
-  ngOnInit() {
+  obj3: any = [{
+    answered: false
+  }];
+
+  obj4: any = {
+    answered: false
   }
+  constructor( 
+    private data: DataService,
+    private rest: RestApiService) { }
+
+    async ngOnInit() {
+      try {
+        const data = await this.rest.get(
+          'http://localhost:3030/api/items'
+        );
+        data['success']
+          ? (this.items = data['items'])
+          : this.data.error(data['message']);
+        //data['success']
+        //  ? (this.question1 = this.items[0])
+        //  : console.log("TTTT error");  
+        if(data['success']){
+          this.question1 = this.items[0];
+          var obj2 = {
+            answered : false
+          }
+          this.items.forEach(element => {
+           element= Object.assign(element, this.obj4 );
+
+           console.log(JSON.stringify(element));
+
+         });
+        //  this.objTEST = Object.assign(this.items, this.obj3 );
+        //  console.log("dfdfd " + this.objTEST);
+        //  console.log(JSON.stringify(this.objTEST));
+       // console.log(JSON.stringify(this.items));
+
+
+        } else {
+          console.log("TTTT error"); 
+        }
+      } catch (error) {
+        this.data.error(error['message']);
+      }
+    }
 
 }
+
+
