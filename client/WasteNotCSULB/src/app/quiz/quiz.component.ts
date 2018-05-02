9bf9e7eb29f83a4a76d4a850427307e76233144a
patrickly <patrickkimly@gmail.com>
2018-05-01 23:17:27 -0700

    Questions are sorted in the quiz

--- client/WasteNotCSULB/src/app/quiz/quiz.component.ts
+++ client/WasteNotCSULB/src/app/quiz/quiz.component.ts
@@ -68,7 +68,7 @@ export class QuizComponent implements OnInit {
            console.log(JSON.stringify(element));
 
          });
-
+         this.shuffle(this.items);
 
         } else {
           console.log("TTTT error"); 
@@ -143,7 +143,29 @@ export class QuizComponent implements OnInit {
         this.quizFinished = true;
        }
   
-    }
+    } //submitAnswer
+
+
+    shuffle(array) : void {
+
+      console.log("444shufffling" );
+      let counter = array.length;
+  
+      // While there are elements in the array
+      while (counter > 0) {
+          // Pick a random index
+          let index = Math.floor(Math.random() * counter);
+  
+          // Decrease counter by 1
+          counter--;
+  
+          // And swap the last element with it
+          let temp = array[counter];
+          array[counter] = array[index];
+          array[index] = temp;
+      }
+  
+  }
 
 
 }
