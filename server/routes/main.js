c04657d28fe1d94b77cfa021738b9be4c973e503
patrickly <patrickkimly@gmail.com>
2018-05-06 08:34:24 -0700

    Can edit an item backend

--- server/routes/main.js
+++ server/routes/main.js
@@ -201,10 +201,13 @@ router.route('/categories')
       });
   });
 
+
+  
+
   router.delete('/item/:id', (req, res, next) => {
 
-      console.log(JSON.stringify(req.body));
-      console.log(req.params.id);
+    //  console.log(JSON.stringify(req.body));
+    //  console.log(req.params.id);
      Item.remove({ _id: req.params.id }, function (err){
        if(err) {
          return handleError(err);
@@ -253,6 +256,55 @@ router.route('/categories')
         message: 'Successfully Added the item',
       });
     });
+
+
+    // status GET: Good but must be logged in
+// status POST: Good 
+router.route('/profile')
+  .get(checkJWT, (req, res, next) => {
+    User.findOne({ _id: req.decoded.user._id }, (err, user) => {
+      res.json({
+        success: true,
+        user: user,
+        message: "Successful"
+      });
+    });
+  })
+  .post(checkJWT, (req, res, next) => {
+    User.findOne({ _id: req.decoded.user._id }, (err, user) => {
+      if (err) return next(err);
+
+      if (req.body.name) user.name = req.body.name;
+      if (req.body.email) user.email = req.body.email;
+      if (req.body.password) user.password = req.body.password;
+
+      user.isAdmin = req.body.isAdmin;
+
+      user.save();
+      res.json({
+        success: true,
+        message: 'Successfully edited your profile'
+      });
+    });
+  });
 */
+
+
+router.post('/item/:id', (req, res, next) => {
+  Item.findOne({ _id: req.params.id }, (err, item) => {
+    if (err) return next(err);
+
+    if (req.body.title) item.title = req.body.title;
+    if (req.body.description) item.description = req.body.description;
+    if (req.body.image) item.image = req.body.image;
+    if(req.body.category) item.category = req.body.category;
+
+    item.save();
+    res.json({
+      success: true,
+      message: 'Successfully edited your item'
+    });
+  });
+});
 
 module.exports = router;
