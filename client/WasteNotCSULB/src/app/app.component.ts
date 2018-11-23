2914772ae8b2af9564e9508d182692b25ddf3332
Patrick Ly <patrickkimly@gmail.com>
2018-11-23 00:41:11 -0800

    Can search for pizza.

--- client/WasteNotCSULB/src/app/app.component.ts
+++ client/WasteNotCSULB/src/app/app.component.ts
@@ -22,7 +22,7 @@ export class AppComponent {
     , 'Georgia', 'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky'
     , 'Louisiana', 'Maine', 'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi',
     'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina',
-    'North Dakota', 'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico',
+    'North Dakota', 'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pizza', 'Pennsylvania', 'Puerto Rico',
     'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Island', 'Virginia', 'Washington',
     'West Virginia', 'Wisconsin', 'Wyoming'];
 
